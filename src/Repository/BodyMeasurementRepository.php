b514d79b85854c184942861c72a26472cfacfc98
Pascal Rieck <pascal.rieck@icloud.com>
2025-11-04 12:55:46 +0100

    implemented BodyMeasurement CRUD functionality II

--- src/Repository/BodyMeasurementRepository.php
+++ src/Repository/BodyMeasurementRepository.php
@@ -3,7 +3,6 @@
 namespace App\Repository;
 
 use App\Entity\BodyMeasurement;
-use App\Entity\Note;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
