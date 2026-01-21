7bad45dff4190e80c61b17d809977e79878d845f
Pascal Rieck <pascal.rieck@icloud.com>
2026-01-21 10:05:05 +0100

    rm bug, where alerts where shown when "listAll" was requested in ExecutionCreateForm.js

--- frontend/src/components/ExecutionCreateForm.js
+++ frontend/src/components/ExecutionCreateForm.js
@@ -1,120 +1,156 @@
 import {useEffect, useState} from "react";
 import axios from "axios";
 import "../styles/ExecutionCreateForm.css";
+import ExecutionListLatest from "./ExecutionListLatest";
+import {useNavigate} from "react-router-dom";
 
 const ExecutionCreateForm = () => {
-    const [repetitions, setRepetitions] = useState(12);
-    const [weight, setWeight] = useState(0);
-    const [isSubmitting, setIsSubmitting] = useState(false);
+        const [repetitions, setRepetitions] = useState(12);
+        const [weight, setWeight] = useState(0);
+        const [isSubmitting, setIsSubmitting] = useState(false);
 
-    const [exercises, setExercises] = useState([]);
-    const [selectedExercise, setSelectedExercise] = useState(null);
+        const [exercises, setExercises] = useState([]);
+        const [selectedExercise, setSelectedExercise] = useState(null);
 
-    useEffect(() => {
-        const fetchExerciseNames = async() => {
-            try {
-                const response = await axios.get("https://backend-fitness-tracker-v5.ddev.site/api/exercise/list");
-                const data = response.data;
-                if (Array.isArray(data)) {
-                    setExercises(data);
-                } else if (typeof data === "object") {
-                    const exerciseArray = Object.values(data).filter((exercise) => typeof exercise === "object");
-                    setExercises(exerciseArray);
-                } else {
-                    console.error("Unexpected data format for exercise ids.", data);
+        const [executions, setExecutions] = useState([]);
+        const [isList, setIsList] = useState(false);
+
+        let navigate = useNavigate();
+        const routeChange = () => {
+            let path = "/executions";
+            setIsList(true);
+            navigate(path);
+        }
+
+        useEffect(() => {
+            const fetchExerciseNames = async () => {
+                try {
+                    const response = await axios.get("https://backend-fitness-tracker-v5.ddev.site/api/exercise/list");
+                    const data = response.data;
+                    if (Array.isArray(data)) {
+                        setExercises(data);
+                    } else if (typeof data === "object") {
+                        const exerciseArray = Object.values(data).filter((exercise) => typeof exercise === "object");
+                        setExercises(exerciseArray);
+                    } else {
+                        console.error("Unexpected data format for exercise ids.", data);
+                    }
+                } catch (error) {
+                    console.error("Error fetching exerciseIds: ", error);
+                    setExercises([]);
                 }
+            }
+            fetchExerciseNames();
+
+            const fetchExecutions = async () => {
+                try {
+                    const response = await axios.get("https://backend-fitness-tracker-v5.ddev.site/api/execution/list");
+                    const data = response.data;
+                    if (Array.isArray(data)) {
+                        setExecutions(data);
+                    } else if (typeof data === "object") {
+                        const executionsArray = Object.values(data).filter((executions) => typeof executions === "object");
+                        setExecutions(executionsArray);
+                    } else {
+                        console.error("Unexpected data format for executions.", data);
+                    }
+                } catch (error) {
+                    console.error("Error fetching Executions: ", error);
+                    setExecutions([]);
+                }
+            }
+            fetchExecutions();
+        }, []);
+
+        const handleSubmit = async (e) => {
+            e.preventDefault();
+            if (!selectedExercise && !isList) {
+                alert("Exercise name selection is required. ");
+                return;
+            }
+
+            setIsSubmitting(true);
+            try {
+                const executionData = {
+                    exerciseName: selectedExercise.uniqueName,
+                    repetitions: repetitions,
+                    weight: weight
+                };
+
+                await axios.post("https://backend-fitness-tracker-v5.ddev.site/api/execution/create", executionData);
+                alert("Execution created successfully");
+                setSelectedExercise("");
             } catch (error) {
-                console.error("Error fetching exerciseIds: ", error);
-                setExercises([]);
+                if (!isList) {
+                    console.error("Error creating execution: ", error);
+                    alert("Failed to create execution. Please try again.");
+                }
+            } finally {
+                setIsSubmitting(false);
             }
         }
-        fetchExerciseNames();
-    }, []);
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        if (!selectedExercise) {
-            alert("Exercise name selection is required. ");
-            return;
-        }
-
-        setIsSubmitting(true);
-        try {
-            const executionData = {
-                exerciseName: selectedExercise.uniqueName,
-                repetitions: repetitions,
-                weight: weight
-            };
-
-            await axios.post("https://backend-fitness-tracker-v5.ddev.site/api/execution/create", executionData);
-            alert("Execution created successfully");
-            setSelectedExercise("");
-        } catch(error) {
-            console.error("Error creating execution: ", error);
-            alert("Failed to create execution. Please try again.");
-        } finally {
-            setIsSubmitting(false);
-        }
-    }
-
-    return (
-        <form onSubmit={handleSubmit} className="form-container">
-            <h2>Create New Execution</h2>
-            <div className="form-group">
-                <label htmlFor="exerciseName" className="label">Exercise:</label>
-                <select
-                    id="exercise"
-                    name="exercise"
-                    value={selectedExercise ? selectedExercise.id : ""}
-                    onChange={(e) => {
-                        const selected = exercises.find((exercise) => exercise.id === parseInt(e.target.value));
-                        console.log(selected);
-                        setSelectedExercise(selected || null);
-                    }}
-                    className="select"
-                >
-                    <option value="" disabled={false}>
-                        Select an exercise
-                    </option>
-                    {exercises.map((exercise) => (
-                        <option key={exercise.id} value={exercise.id}>
-                            {exercise.id} {exercise.uniqueName}
+        return (
+            <form onSubmit={handleSubmit} className="form-container">
+                <h2>Create New Execution</h2>
+                <div className="form-group">
+                    <label htmlFor="exerciseName" className="label">Exercise:</label>
+                    <select
+                        id="exercise"
+                        name="exercise"
+                        value={selectedExercise ? selectedExercise.id : ""}
+                        onChange={(e) => {
+                            const selected = exercises.find((exercise) => exercise.id === parseInt(e.target.value));
+                            console.log(selected);
+                            setSelectedExercise(selected || null);
+                        }}
+                        className="select"
+                    >
+                        <option value="" disabled={false}>
+                            Select an exercise
                         </option>
-                    ))}
-                </select>
-            </div>
-            <div className="form-group">
-                <label htmlFor="title" className="label">Repetitions:</label>
-                <input
-                    type="number"
-                    id="repetitions"
-                    name="repetitions"
-                    value={repetitions}
-                    onChange={(e) => setRepetitions(parseInt(e.target.value))}
-                    placeholder="set reps"
-                    className="input"
-                />
-            </div>
-            <div className="form-group">
-                <label htmlFor="title" className="label">Weight in kg:</label>
-                <input
-                    type="number"
-                    id="weight"
-                    name="weight"
-                    value={weight}
-                    onChange={(e) => setWeight(parseInt(e.target.value))}
-                    placeholder="set weight"
-                    className="input"
-                />
-            </div>
-            <button
-                type="submit"
-                disabled={isSubmitting}
-                className={`button ${isSubmitting ? 'disabled' : 'enabled'}`}
-            >
-                {isSubmitting ? "Creating..." : "Create Execution"}
-            </button>
-        </form>
-    );
-};
+                        {exercises.map((exercise) => (
+                            <option key={exercise.id} value={exercise.id}>
+                                {exercise.id} {exercise.uniqueName}
+                            </option>
+                        ))}
+                    </select>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="title" className="label">Repetitions:</label>
+                    <input
+                        type="number"
+                        id="repetitions"
+                        name="repetitions"
+                        value={repetitions}
+                        onChange={(e) => setRepetitions(parseInt(e.target.value))}
+                        placeholder="set reps"
+                        className="input"
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="title" className="label">Weight in kg:</label>
+                    <input
+                        type="number"
+                        id="weight"
+                        name="weight"
+                        value={weight}
+                        onChange={(e) => setWeight(parseInt(e.target.value))}
+                        placeholder="set weight"
+                        className="input"
+                    />
+                </div>
+                <button
+                    type="submit"
+                    disabled={isSubmitting}
+                    className={`button ${isSubmitting ? 'disabled' : 'enabled'}`}
+                >
+                    {isSubmitting ? "Creating..." : "Create Execution"}
+                </button>
+                <ExecutionListLatest executions={executions}></ExecutionListLatest>
+                <button color="primary" className="px-4" onClick={routeChange} typeof="button">List all</button>
+            </form>
+        );
+    }
+;
 export default ExecutionCreateForm;
